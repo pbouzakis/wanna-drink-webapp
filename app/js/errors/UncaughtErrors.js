1be9a9d1b81dd8cf3be2178e00d44ec510d063c7
Paul Bouzakis <github@paulbouzakis.com>
2016-03-18 07:20:44 -0400

    Implement a spak UncaughtErrors provider.
    This will replace the current default provider.
    Currently just puts an alert up when we get any uncaught error in the system.

---
+++ app/js/errors/UncaughtErrors.js
@@ -0,0 +1,33 @@
+import "tracekit"; // Create global TraceKit ref.
+import { autobind } from "core-decorators";
+import { App } from "spak";
+import { logger } from "spak/decorators";
+
+@logger("uncaught-errors")
+export default class UncaughtErrors {
+    listen() {
+        TraceKit.collectWindowErrors = true;
+        TraceKit.remoteFetching = false;
+        // Tell trackeit to let us know about window.onerror
+        TraceKit.report.subscribe(this._handleReport);
+    }
+
+    handleActionError(error) {
+        this.handleSystemError(error, "Action");
+    }
+
+    handleSystemError(error, type = "System") {
+        this._logAndReportError(error, type);
+    }
+
+    _logAndReportError(error, type) {
+        this.logger.info(`Handling: ${type} error.`);
+        this._handleReport(error);
+    }
+
+    @autobind
+    _handleReport(error) {
+        this.logger.error(error);
+        alert(`Oops! ${error.message}.`);
+    }
+}
