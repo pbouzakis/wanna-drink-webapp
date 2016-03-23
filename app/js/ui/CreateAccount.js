2d985e874caa37d465bb90c387bd015586431ece
c-h-russell-walker <c.russell.walker@gmail.com>
2016-03-23 13:08:13 -0400

    Second WIP commit for geolocation/zip-lat-lng data

--- app/js/ui/CreateAccount.js
+++ app/js/ui/CreateAccount.js
@@ -7,7 +7,9 @@ export default class CreateAccount extends React.Component {
     constructor() {
         super();
         this.state = {
-            zipcode: ''
+            zipcode: '',
+            latitude: '',
+            longitude: '',
         };
     }
 
@@ -34,6 +36,8 @@ export default class CreateAccount extends React.Component {
                         <i className="material-icons small" onClick={this._handleZipCodeClick}>location_on</i>
                         <label htmlFor="zipcode">Zip Code</label>
                         <input type="text" id="zipcode" name="zipcode" value={this.state.zipcode} />
+                        <input type="text" id="latitude" className="hide" name="latitude" value={this.state.latitude} />
+                        <input type="text" id="longitude" className="hide" name="longitude" value={this.state.longitude} />
                         <label htmlFor="fav-styles">Favorite Styles</label>
                         <select id="fav-styles" name="fav-styles">
                             <option value="" disabled>Choose your option</option>
@@ -62,55 +66,64 @@ export default class CreateAccount extends React.Component {
         this.props.onBack();
     }
 
+    @autobind
+    _callGeolocationCurrentPos(callback) {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                callback(position.coords.latitude, position.coords.longitude);
+            });
+        } else {
+            console.warn('Geolocation not available');
+        }
+    }
+
+    @autobind
+    _handleGeocoderData(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                try {
+                    var address = results[0].address_components;
+                    var zipcode = address[address.length - 1].long_name;
+                    this.setState({ zipcode });
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+            if (results[1]) {
+                try {
+                    var loc = results[1].geometry.location;
+                    this.setState({
+                        latitude: loc.lat(),
+                        longitude: loc.lng(),
+                    });
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        } else {
+            console.error('Geocoder failed due to: ' + status);
+        }
+    }
+
+    @autobind
+    _handleGeolocate(lat, lng) {
+        var geocoder = new google.maps.Geocoder,
+            latLong = { lat, lng };
+        geocoder.geocode({'location': latLong}, this._handleGeocoderData);
+    }
+
     @autobind @preventDefault
     _handleZipCodeClick() {
-        // People will know their zipcode which we can save - but also maybe we want a lat/long really?
         // Our API backend will want to use a Zipcode to get a lat long to save &
             // potentially display a map to confirm with the user
         // Reason being is we'll want a lat/long to work off of usually - like when user is 'mobile'
             // and wants to see availability of their preferences where they are on their phone etc.
 
-        // tl;dr - We need a zipcode input for UX reason or a 'get zipcode' button
-        // we actually really care about the lat/long but will populate the zipcode field
-        // which will implicitly act as confirmation from the user
-        // if they enter zipcode we'll have an approximate lat/long which we can use for the time being
+        // We need a zipcode input for UX reason or a 'get zipcode' button
+            // we actually really care about the lat/long but will populate the zipcode field
+            // which will implicitly act as confirmation from the user
+        // If they only enter a zipcode we'll get a lat/long on the back end
 
-        var geocoder = new google.maps.Geocoder,
-            // TODO - set up browser geoloaction callback which will give us lat/long
-            lat = '40.7083161',
-            lng = '-73.9474741';
-        var latLong = {lat: parseFloat(lat), lng: parseFloat(lng)};
-
-        // TODO - This seems incorrect ;)
-        var that = this;
-
-        geocoder.geocode({'location': latLong}, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                    try {
-                        var address = results[0].address_components;
-                        var zipcode = address[address.length - 1].long_name;
-                        // TODO - Address use of 'that' var
-                        that.setState({ zipcode });
-                    } catch (e) {
-                        console.error(e);
-                    }
-                }
-                if (results[1]) {
-                    try {
-                        var loc = results[1].geometry.location;
-                        var lat = loc.lat(),
-                            lng = loc.lng();
-                        console.log("This is where we want to save lat long:");
-                        console.log(lat);
-                        console.log(lng);
-                    } catch (e) {
-                        console.error(e);
-                    }
-                }
-            } else {
-                // window.alert('Geocoder failed due to: ' + status);
-            }
-        });
+        this._callGeolocationCurrentPos(this._handleGeolocate);
     }
 }
